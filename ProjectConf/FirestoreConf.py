eff606267c9f9f830b4a5e90deda3a46acb12f0b
Piyush Garg <22243371+piyush888@users.noreply.github.com>
2022-05-18 00:28:47 -0700

    Deployment Phase 1.1
    - Added service Account Key as Secrets to Pods
    - For testing, create firebase-secret

--- ProjectConf/FirestoreConf.py
+++ ProjectConf/FirestoreConf.py
@@ -1,10 +1,17 @@
+import os
+import json
 import asyncio
 import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 from google.cloud.firestore import AsyncClient
 
-cred = credentials.Certificate('serviceAccountKey.json')
+firebase_cred = json.loads(os.getenv("FIREBASE_CRED")) if os.getenv("FIREBASE_CRED") else None
+
+if firebase_cred:
+    cred = credentials.Certificate(firebase_cred)
+else:
+    cred = credentials.Certificate('serviceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred)
 # db = firestore.client(app = app)
 async_db = AsyncClient(credentials=default_app.credential.get_credential(), project=default_app.project_id)
