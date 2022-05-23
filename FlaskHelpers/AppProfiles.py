d3f475f366996b909c5a945c2ef713c57463297c
Kshitiz Sharma <kshitizsharmav@gmail.com>
2022-05-22 21:13:59 -0400

    Profiles Write Through Cache

---
+++ FlaskHelpers/AppProfiles.py
@@ -0,0 +1,46 @@
+import flask
+from flask import Blueprint, current_app, jsonify, request
+import json
+import logging
+import time
+import requests
+
+from ProjectConf.AsyncioPlugin import run_coroutine
+from ProjectConf.AuthenticationDecorators import validateCookie
+from ProjectConf.ReadFlaskYaml import cachingServerRoute, headers
+
+
+app_report_profile = Blueprint('appProfiles', __name__)
+logger = logging.getLogger()
+
+
+# Invoked on Report Profile request from a user.
+@current_app.route('/storeProfileInBackend', methods=['POST'])
+@validateCookie
+def post_profile_to_backend(decoded_claims=None):
+    """
+    Endpoint to store likes, superlikes, dislikes, liked_by, disliked_by, superliked_by for users
+        Body of Request contains following payloads:
+        - current user id
+        - reported profile id
+    """
+    try:
+        userId = decoded_claims['user_id']
+        current_app.logger.info(userId)
+        requestData = {
+            "profile": request.json['profile'],
+        }
+        response = requests.post(f"{cachingServerRoute}/storeProfileInBackendGate",
+                                 data=json.dumps(requestData),
+                                 headers=headers)
+        current_app.logger.info(f"{userId}: Sent successfully from /storeProfileInBackend")
+        response = jsonify({'message':"Success"})
+        response.status_code = 200
+        return response
+    except Exception as e:
+        current_app.logger.error(f"{userId}: Unable to to caching service for storage /storeProfileInBackend")
+        current_app.logger.exception(e)
+        response = jsonify({'message': 'An error occured in API /storeProfileInBackend'})
+        response.status_code = 400
+        return response
+        
--- app.py
+++ app.py
@@ -15,6 +15,7 @@ with app.app_context():
     from FlaskHelpers.AppSuperLikesDislikes import app_super_likes_dislikes
     from FlaskHelpers.AppSwipeView import app_swipe_view_app
     from FlaskHelpers.AppUnswipe import app_unswipe
+    from FlaskHelpers.AppProfiles import post_profile_to_backend
 
 @app.before_first_request
 def setup_logging():
@@ -42,4 +43,5 @@ def test():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
+    #app.run(host="127.0.0.1", port=5040, debug=True)
     app.logger.info("Starting Amore Flask")
